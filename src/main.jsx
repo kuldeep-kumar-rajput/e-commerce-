3625ac663e97ac177b940b5d05aef5486adcfaa3
Kuldeep kumar <kuldeepk78079@gmail.com>
2026-03-13 18:05:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,24 @@
-import { lazy, Suspense } from "react";
-import { StrictMode } from "react";
+import { lazy, Suspense, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./index.css";
 import App from "./App.jsx";
 import About from "./page/About.jsx";
 import Product from "./page/Product.jsx";
 import Cart from "./page/Cart.jsx";
 import Home from "./page/Home.jsx";
+
 const Contact = lazy(() => import("./page/Contact.jsx"));
-const products = lazy(() => import("./page/Products.jsx"));
+const Products = lazy(() => import("./page/Products.jsx"));
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      {
-        index: true, 
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
       {
         path: "contact",
         element: (
@@ -34,28 +29,18 @@ const router = createBrowserRouter([
           </Suspense>
         ),
       },
-      {
-        path: "/product-details/:id",
-        element: <Product />,
-      },
-      {
-        path: "cart",
-        element: <Cart />,
-      },
+      { path: "product-details/:id", element: <Product /> },
+      { path: "cart", element: <Cart /> },
       {
         path: "products",
         element: (
           <Suspense
-            fallback={<p className="text-center mt-10">Loading products...</p>}
+            fallback={<p className="text-center mt-10">Loading Products...</p>}
           >
             <Products />
           </Suspense>
         ),
       },
-      {
-    path: "*",
-    element: <h1>Page Not Found</h1>,
-  }
     ],
   },
 ]);
@@ -64,6 +49,4 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-  
-
-  
+);
